0fdde781a86b18f38f17a12e75ca6ada591e30aa
NanaMuslim <thegreatbanana17@gmail.com>
2020-07-05 12:43:41 +0600

    Изменил If на свитч

--- index.js
+++ index.js
@@ -9,15 +9,30 @@ app.use(bodyParser.json());
 // Определяем обработчик корневого урла с методом POST
 app.post("/", function(request, response) {
     const { firstArg, secondArg, operation } = request.body; // деструктуризация
-    if (operation === '+') {
-        response.status(200).json(firstArg + secondArg);
-    } else if (operation === '-') {
-        response.status(200).json(firstArg - secondArg);
-    } else if (operation === '/') {
-        response.status(200).json(firstArg / secondArg);
-    } else if (operation === '*') {
-        response.status(200).json(firstArg * secondArg);
+    switch(operation){
+        case '+':
+            response.status(200).json(firstArg + secondArg);
+            break;
+        case '-':
+            response.status(200).json(firstArg - secondArg);
+            break;
+        case '*':
+            response.status(200).json(firstArg * secondArg);
+            break;
+        case '/':
+            response.status(200).json(firstArg / secondArg);
+            break;
     }
+    
+    // if (operation === '+') {
+    //     response.status(200).json(firstArg + secondArg);
+    // } else if (operation === '-') {
+    //     response.status(200).json(firstArg - secondArg);
+    // } else if (operation === '/') {
+    //     response.status(200).json(firstArg / secondArg);
+    // } else if (operation === '*') {
+    //     response.status(200).json(firstArg * secondArg);
+    // }
 });
 // начинаем прослушивать подключения на 3000 порту
-app.listen(3000);
+app.listen(3001);
